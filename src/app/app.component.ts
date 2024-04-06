1fa67e95f33cab2ad0abb2ab26e276ce6907d6d3
Sergio <sergio.ume@hotmail.com>
2024-04-06 19:25:50 +0100

    Estilos boostrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Tarea2.3_sergio';
+  title = 'Calculadora';
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+
+  sumar():void{
+    this.resultado=this.numero1+this.numero2;
+
+
+    //otras maneras
+    //this.resultado=+this.numero1+ +this.numero2; //se agrega + para evitar concatenacion de strings si en el 
+    //formulario el input type="text"
+    //this.resultado=parseInt(this.numero1)+parseInt(this.numero2);
+    //habria que poner los nuemeros declarados como strings
+  }
+
+  restar(){
+    this.resultado=this.numero1-this.numero2;
+  }
 }
